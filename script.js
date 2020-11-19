1866b8c533a30b657a95d954c6facebd25b4c2fa
verja <janneth_verduzco@live.com>
2020-11-18 19:57:55 -0600

    fixed query URL link in js file

--- script.js
+++ script.js
@@ -8,25 +8,31 @@ $(document).ready(function() {
 // LISTENERS
 // =======================================================================================
 $("#searchBtn").on("click", function(event) {
-    event.preventDefault()
+    event.preventDefault();
     var citySearch = $("#city-search").val()
-    console.log(citySearch)
+    // console.log(citySearch)
     cityWeather(citySearch)
 })
+
+
 
 // FUNCTIONS
 // =======================================================================================
 
 function cityWeather(city) {
-    var queryURL ="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=37849e55e443f85f3dcbbc367fbd40e9";
-    console.log(queryURL)
+    var apiKey = "&appid=463e991c43d34445395b82d02c5b4287" 
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+    // console.log(queryURL)
 
     $.ajax({
         url: queryURL,
-        method: "GET" 
-    }).then(function(weatherRes){
-        console.log(weatherRes)
-    })
+        method: "GET"
+      }).then(function(cityWeather) {
+        console.log(queryURL)
+
+
+    
+      });
     
 } 
 
