fe77497f3a29ea7d2db13605cda5d63cad7a9c1e
verja <janneth_verduzco@live.com>
2020-11-19 00:18:52 -0600

    added ajax call to js file

--- script.js
+++ script.js
@@ -1,40 +1,75 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
-// VARIABLES
-// =======================================================================================
+    // VARIABLES
+    // =======================================================================================
 
 
-// LISTENERS
-// =======================================================================================
-$("#searchBtn").on("click", function(event) {
-    event.preventDefault();
-    var citySearch = $("#city-search").val()
-    // console.log(citySearch)
-    cityWeather(citySearch)
-})
+    // LISTENERS
+    // =======================================================================================
+    $("#searchBtn").on("click", function (event) {
+        event.preventDefault()
+        var citySearch = $("#city-search").val()
+        // console.log(citySearch)
+        cityWeather(citySearch)
+    })
 
+    // FUNCTIONS
+    // =======================================================================================
 
+    function cityWeather(city) {
+        var apiKey = "&appid=463e991c43d34445395b82d02c5b4287"
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+        // console.log(queryURL)
 
-// FUNCTIONS
-// =======================================================================================
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (weatherRes) {
 
-function cityWeather(city) {
-    var apiKey = "&appid=463e991c43d34445395b82d02c5b4287" 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
-    // console.log(queryURL)
+            var temp = ((weatherRes.main.temp - 273.15) * 1.80 + 32).toFixed(2);
+            // console.log(temp)
+            var humidity = weatherRes.main.humidity;
+            // console.log(humidity)
+            var windSpeed = weatherRes.wind.speed;
+            // console.log(windSpeed)
+            var lon = weatherRes.coord.lon;
+            // console.log(location)
+            var lat = weatherRes.coord.lat;
+            // console.log(lat)
+            cityUV(lat, lon)
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(cityWeather) {
-        console.log(queryURL)
+             $(".temp").text(temp);
+             $(".humidity").text(humidity);
+             $(".wind-speed").text(windSpeed);
+             
+
+        })
+        
+    }
+
+    function cityUV(lat, lon) {
+        var apiKey = "&appid=463e991c43d34445395b82d02c5b4287"
+        var queryURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + apiKey;
+        // console.log(queryURL)
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (coordRes) {
+            // console.log(coordRes)
+            var uvIndex = coordRes.value;
+            // console.log(uvIndex)
+            $(".uv-index").text(uvIndex)
+        })
+
+        
+
+    }
+
 
 
     
-      });
-    
-} 
 
 
 
